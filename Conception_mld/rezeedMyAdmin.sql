8bb003816264f6811183286cc8d26566c4079f91
Vazn <cavazzoni.chris@gmail.com>
2022-01-26 16:06:22 +0100

    Min

--- Conception_mld/rezeedMyAdmin.sql
+++ Conception_mld/rezeedMyAdmin.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jan 26, 2022 at 03:01 PM
+-- Generation Time: Jan 26, 2022 at 03:05 PM
 -- Server version: 8.0.27
 -- PHP Version: 7.4.26
 
@@ -24,82 +24,13 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `customize`
---
-
-CREATE TABLE `customize` (
-  `id_user` int NOT NULL,
-  `id_skin` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `form`
---
-
-CREATE TABLE `form` (
-  `id_track` int NOT NULL,
-  `id_playlist` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `genre`
---
-
-CREATE TABLE `genre` (
-  `id_genre` int NOT NULL,
-  `name_genre` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `playlist`
---
-
-CREATE TABLE `playlist` (
-  `id_playlist` int NOT NULL,
-  `id_user` int DEFAULT NULL,
-  `id_genre` int DEFAULT NULL,
-  `name_playlist` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `skin`
---
-
-CREATE TABLE `skin` (
-  `id_skin` int NOT NULL,
-  `name_skin` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `track`
---
-
-CREATE TABLE `track` (
-  `id_track` int NOT NULL,
-  `name_track` varchar(50) DEFAULT NULL,
-  `url_track` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `user`
 --
 
 CREATE TABLE `user` (
   `id_user` int NOT NULL,
-  `firstName_user` varchar(50) DEFAULT NULL,
-  `lastName_user` varchar(50) DEFAULT NULL,
+  `name_user` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `first_name_user` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
   `login_user` varchar(50) DEFAULT NULL,
   `pw_user` varchar(100) DEFAULT NULL,
   `preferences_user` varchar(255) DEFAULT NULL
@@ -108,46 +39,6 @@ CREATE TABLE `user` (
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `customize`
---
-ALTER TABLE `customize`
-  ADD PRIMARY KEY (`id_skin`,`id_user`),
-  ADD KEY `fk_user_customize` (`id_user`);
-
---
--- Indexes for table `form`
---
-ALTER TABLE `form`
-  ADD PRIMARY KEY (`id_track`,`id_playlist`),
-  ADD KEY `fk_playlist_form` (`id_playlist`);
-
---
--- Indexes for table `genre`
---
-ALTER TABLE `genre`
-  ADD PRIMARY KEY (`id_genre`);
-
---
--- Indexes for table `playlist`
---
-ALTER TABLE `playlist`
-  ADD PRIMARY KEY (`id_playlist`),
-  ADD KEY `fk_user_playlist` (`id_user`),
-  ADD KEY `fk_genre_playlist` (`id_genre`);
-
---
--- Indexes for table `skin`
---
-ALTER TABLE `skin`
-  ADD PRIMARY KEY (`id_skin`);
-
---
--- Indexes for table `track`
---
-ALTER TABLE `track`
-  ADD PRIMARY KEY (`id_track`);
 
 --
 -- Indexes for table `user`
@@ -160,59 +51,10 @@ ALTER TABLE `user`
 --
 
 --
--- AUTO_INCREMENT for table `genre`
---
-ALTER TABLE `genre`
-  MODIFY `id_genre` int NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `playlist`
---
-ALTER TABLE `playlist`
-  MODIFY `id_playlist` int NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `skin`
---
-ALTER TABLE `skin`
-  MODIFY `id_skin` int NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `track`
---
-ALTER TABLE `track`
-  MODIFY `id_track` int NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
   MODIFY `id_user` int NOT NULL AUTO_INCREMENT;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `customize`
---
-ALTER TABLE `customize`
-  ADD CONSTRAINT `fk_skin_customize` FOREIGN KEY (`id_skin`) REFERENCES `skin` (`id_skin`),
-  ADD CONSTRAINT `fk_user_customize` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`);
-
---
--- Constraints for table `form`
---
-ALTER TABLE `form`
-  ADD CONSTRAINT `fk_playlist_form` FOREIGN KEY (`id_playlist`) REFERENCES `playlist` (`id_playlist`),
-  ADD CONSTRAINT `fk_track_form` FOREIGN KEY (`id_track`) REFERENCES `track` (`id_track`);
-
---
--- Constraints for table `playlist`
---
-ALTER TABLE `playlist`
-  ADD CONSTRAINT `fk_genre_playlist` FOREIGN KEY (`id_genre`) REFERENCES `genre` (`id_genre`),
-  ADD CONSTRAINT `fk_user_playlist` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
