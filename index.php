6dcd49991bcb93eba9f4d35c5ef8f8e392930405
Vazn <cavazzoni.chris@gmail.com>
2022-01-26 16:29:11 +0100

    Some cleaning

--- index.php
+++ index.php
@@ -1,47 +1,17 @@
 <?php
+    // $_SESSION Pour gérer les sessions
     require "./indexVue.php";
     require "./model/connect.php";
     require "./model/preparedQueries.php";
+    require "./model/userQueries.php";
 
-    // $_SESSION
-    
     if (isset($_GET["type"])) {
         if ($_GET["type"] === "login") {
-            $condition = isset($_POST['login_user']) && 
-            isset($_POST['pw_user']);
-            
-            if ($condition) {
-                $loginUser = $_POST['login_user'];
-                $password = $_POST['pw_user'];                  // ! Hash password here
-                
-            }
+            connect($preparedQuery);
         }
         
         if ($_GET["type"] === "register") {
-            $condition = isset($_POST['name_user']) &&
-                        isset($_POST['first_name_user']) &&
-                        isset($_POST['login_user']) &&
-                        isset($_POST['pw_user']) &&
-                        isset($_POST['email_user']) &&
-                        isset($_POST['preferences_user']);
-
-            if ($condition) {
-                $nameUser = $_POST['name_user'];
-                $firstNameUser = $_POST['first_name_user'];
-                $loginUser = $_POST['login_user'];
-                $password = $_POST['pw_user'];                  // ! Hash password here
-                $email = $_POST['email_user'];
-                $preferencesUser = $_POST['preferences_user'];
-
-                queryDatabase($createUser, array(
-                    ':name_user' => $_POST['name_user'],
-                    ':first_name_user' => $_POST['first_name_user'],
-                    ':login_user' => $_POST['login_user'],
-                    ':pw_user' => $_POST['pw_user'],
-                    ':email_user' => $_POST['email_user'],
-                    ':preferences_user' => $_POST['preferences_user']
-                ));
-            }
+            register($createUser);
         }
     }
 ?>
---
+++ phpControlers/userQueries.php
@@ -0,0 +1,37 @@
+<?php
+   function register($preparedQuery) {
+      $condition = (
+         isset($_POST['name_user']) &&
+         isset($_POST['first_name_user']) &&
+         isset($_POST['login_user']) &&
+         isset($_POST['pw_user']) &&
+         isset($_POST['email_user']) &&
+         isset($_POST['preferences_user'])
+      );
+
+      if ($condition) {
+         $password = $_POST['pw_user'];                  // ! Hash password here
+
+         queryDatabase($preparedQuery, array(
+            ':name_user' => $_POST['name_user'],
+            ':first_name_user' => $_POST['first_name_user'],
+            ':login_user' => $_POST['login_user'],
+            ':pw_user' => $password,
+            ':email_user' => $_POST['email_user'],
+            ':preferences_user' => $_POST['preferences_user']
+         ));
+      }
+   }
+
+   function connect($preparedQuery) {
+      $condition = (
+         isset($_POST['login_user']) && 
+         isset($_POST['pw_user'])
+      );
+      
+      if ($condition) {
+         $loginUser = $_POST['login_user'];
+         $password = $_POST['pw_user'];                  // ! Hash password here  
+      }
+   }
+?>
