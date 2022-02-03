1f6dc8961f3127b1cd4d9d7d64f3e51bee211ff5
Vazn <cavazzoni.chris@gmail.com>
2022-02-03 11:56:31 +0100

    Playlist chocies, quite ok

--- scripts/player.js
+++ scripts/player.js
@@ -1,5 +1,6 @@
-(function player() {
 
+(function player() {
+   
    equalizer();                // Front modules
    logIn();
    trackControls();
@@ -7,15 +8,34 @@
    logOut();
 })();
 
+
 async function trackControls() {
    const root = "https://www.youtube.com/embed/";
-   const userGenre = JSON.parse(localStorage.getItem('UserData')).genre;
    const iframe = document.querySelector("iframe");
+   const select = document.querySelector("select")
    const buttons = Array.from(document.querySelectorAll(".music_button"));
-   
-   await queryControler("playlist", userGenre);             // Requête au controlleur
-   const playlistLink = JSON.parse(localStorage.getItem('PlaylistData'));
+   const userGenre = JSON.parse(localStorage.getItem('UserData')).genre;
 
+   await queryControler("playlist", userGenre);             // Requête au controlleur
+
+   select.addEventListener("change", function (e) {
+      queryControler("playlist", e.target.value);
+
+      const playlistLink = JSON.parse(localStorage.getItem('PlaylistData'));
+      iframe.setAttribute("src", `${root}${playlistLink.track_1[1]}`);
+   
+      for (let i = 0; i < buttons.length; i++) {
+         buttons[i].setAttribute("Value", playlistLink[`track_${i + 1}`][0]);
+
+         buttons[i].addEventListener("click", (e) => {
+            e.preventDefault();
+            iframe.setAttribute("src", `${root}${playlistLink[`track_${i + 1}`][1]}`);
+         })
+      }
+   });
+
+
+   const playlistLink = JSON.parse(localStorage.getItem('PlaylistData'));
    iframe.setAttribute("src", `${root}${playlistLink.track_1[1]}`);
    
    for (let i=0 ; i<buttons.length ; i++) {
